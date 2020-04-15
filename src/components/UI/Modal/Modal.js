cb3284e52ef09bc19054b5e673de7b1f68700786
Jonhnes Lopes <jonhnes@gmail.com>
2020-04-15 18:21:50 -0300

    Only updating modal when it shows

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,17 +4,29 @@ import classes from "./Modal.module.css";
 import Aux from "../../../hoc/Aux";
 import Backdrop from "../Backdrop/Backdrop";
 
-const Modal = (props) => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed}/>
-    <div className={classes.Modal}
-         style={{
-           transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-           opacity: props.show ? '1' : '0'
-         }}>
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends React.Component {
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    return nextProps.show !== this.props.show;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('updating!!!')
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+        <div className={classes.Modal}
+             style={{
+               transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+               opacity: this.props.show ? '1' : '0'
+             }}>
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
 
 export default Modal;
