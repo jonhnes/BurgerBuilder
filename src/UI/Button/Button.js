41b45a0de9ba9f4578b2740e200ea5ef92bc272e
Jonhnes Lopes <jonhnes@gmail.com>
2020-04-15 11:28:02 -0300

    Creating button component

---
+++ src/UI/Button/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from "./Button.module.css";
+
+const Button = (props) => {
+  const buttonClasses = [classes.Button, classes[props.btnType]].join('')
+  return (
+    <button onClick={props.clicked}
+            className={buttonClasses}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
