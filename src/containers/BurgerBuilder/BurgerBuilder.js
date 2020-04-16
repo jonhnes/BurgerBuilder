af1b2d6e98ce5c747b652f46ae5540e7f7b350db
Jonhnes Lopes <jonhnes@gmail.com>
2020-04-16 12:24:56 -0300

    Sending order to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -67,7 +68,25 @@ class BurgerBuilder extends Component {
 
   purchaseCancelHandler = () => this.setState({purchasing: false})
 
-  purchaseContinueHandler = () => alert('You continue!');
+  purchaseContinueHandler = () => {
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Jonhnes Lopes',
+        address: {
+          street: 'Teststreet 1',
+          zipCode: '41351',
+          country: 'Germany'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
+    }
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
+  };
 
   render() {
     const disableInfo = {
