1143cc29834f262193f66a0465d8ab316363309b
Jonhnes Lopes <jonhnes@gmail.com>
2020-04-19 07:56:08 -0300

    Creating ContactData

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import CheckoutSummary
   from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import {Route} from "react-router-dom";
+import ContactData from "./ContactData/ContactData";
 
 class Checkout extends Component {
   state = {
@@ -10,6 +12,15 @@ class Checkout extends Component {
       cheese: 1,
       bacon: 1
     }
+  }
+
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {}
+    for (const param of query.entries()) {
+      ingredients[param[0]] = +param[1]
+    }
+    this.setState({ingredients: ingredients});
   }
 
   checkoutCancelledHandler = () => {
@@ -26,6 +37,8 @@ class Checkout extends Component {
         <CheckoutSummary ingredients={this.state.ingredients}
                          checkoutCancelled={this.checkoutCancelledHandler}
                          checkoutContinued={this.checkoutContinuedHandler}/>
+        <Route path={this.props.match.path + '/contact-data'}
+               component={ContactData}/>
       </div>
     );
   }
